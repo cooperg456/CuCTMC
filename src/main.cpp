f4df3ef65d147ddeb107998184a7fc68edc302c9
cooperg456 <cooperg456@users.noreply.github.com>
2026-07-11 08:38:56 +0000

    added cli interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
 #include <fstream>
 
+#include "CLI/CLI11.hpp"
+
 #include "StochasticSimulation.cuh"
 #include "ReactionNetwork.hpp"
  
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		throw std::runtime_error("json file not provided");
-	}
+    CLI::App app{"CuCTMC"};
 
-    ReactionNetwork network(argv[1]);
+    std::string networkFile;
+    app.add_option("input", networkFile, "Reaction network JSON file")->required();
 
     SSASimInfo info{};
-    info.initialConditions = {1000, 3, 0, 0};
-    info.samplePaths = 256;
+
+    app.add_option("--samplePaths", info.samplePaths, "Number of sample paths");
+    app.add_option("--savedPaths", info.savedPaths, "Number of paths to save trajectories for");
+    app.add_option("--tGrid", info.tGrid, "Time grid spacing for saved trajectories");
+    app.add_option("--tMax", info.tMax, "Maximum simulation time");
+    app.add_option("--ic", info.initialConditions, "Initial conditions, e.g. --ic 1000 3 0 0");
+
+    std::vector<std::string> rateOverrides;
+    app.add_option("--rate", rateOverrides, "Override a reaction rate by name, e.g. --rate '/gamma_V=15.0'");
+
+    CLI11_PARSE(app, argc, argv);
+
+    ReactionNetwork network(networkFile);
+
+    for (auto& kv : rateOverrides) {
+        auto pos = kv.find('=');
+        if (pos == std::string::npos) {
+            throw std::runtime_error("bad --rate format, expected name=value: " + kv);
+        }
+
+        std::string name = kv.substr(0, pos);
+        double value = std::stod(kv.substr(pos + 1));
+
+        auto it = std::find(network.reactions.begin(), network.reactions.end(), name);
+        if (it == network.reactions.end()) {
+            throw std::runtime_error("unknown reaction name: " + name);
+        }
+
+        size_t idx = std::distance(network.reactions.begin(), it);
+        network.reactionRates[idx] = value;
+    }
 
     SSASimOutput out = stochasticSimulation(network, info);
 
